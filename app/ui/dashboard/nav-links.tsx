82c8d2591c93f297c39c95f6a5b0bd9c8abd02a4
Cristian <cristian.sulbaran.r@gmail.com>
2024-04-02 14:50:05 -0300

    modificaciones en las fonts; también se añadieron layouts y páginas para mostrar en distintas rutas

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -2,6 +2,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  MagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
 
 // Map of links to display in the side navigation.
@@ -14,6 +15,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Test', href: '/dashboard/test', icon: MagnifyingGlassIcon },
 ];
 
 export default function NavLinks() {
